66e5e6d6beef60415b5fe0a81458e249df3cac29
021Skyfall <021skyfall@gmail.com>
2023-05-08 17:20:13 +0900

    Setting : Base Project

---
+++ server/origin/src/main/java/com/wrieating/origin/OriginApplication.java
@@ -0,0 +1,13 @@
+package com.wrieating.origin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriginApplication.class, args);
+	}
+
+}
---
+++ server/origin/src/test/java/com/wrieating/origin/OriginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrieating.origin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
