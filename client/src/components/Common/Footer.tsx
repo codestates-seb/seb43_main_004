ce3435a08fcd63365a5ea2011dfdbda295fa286e
YujunSun0 <cons202@gmail.com>
2023-05-10 11:31:21 +0900

    Feat : footer 컴포넌트 제작

--- client/src/components/Common/Footer.tsx
+++ client/src/components/Common/Footer.tsx
@@ -2,31 +2,61 @@ import React from 'react'
 import styled from 'styled-components'
 
 const FooterWrapper = styled.footer`
+  position: relative;
+  transform: translateY(-100%);
+  height: 164px;
   font-weight: 500;
   font-size: 14px;
   width: 100%;
   background-color: var(--color-primary);
-  height: 164px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   padding: 24px;
-  border: 1px solid black;
 
   p {
     margin-bottom: 5px;
+  }
+
+  p:first-child {
+    margin-bottom: 9px;
+  }
+
+  a {
+    font-size: 16px;
+    font-weight: 700;
+    transition: all 0.2s linear;
+  }
+
+  a:hover {
+    transform: scale(1.1);
+  }
+
+  strong {
+    font-weight: 700;
   }
 `
 
 const Footer = () => {
   return (
     <FooterWrapper>
-      <p>개발자</p>
+      <p>
+        <strong>개발자</strong>
+      </p>
       <p>프론트 : 선유준 이진하 박혜원</p>
       <p>백엔드 : 이용석 김석현 임채영</p>
-      <p>Copyright 2023. 모개숲 all rights reserved.</p>
-      <p>프로젝트 깃허브 바로가기</p>
+      <p>
+        <strong>Copyright 2023 모개숲 all rights reserved</strong>
+      </p>
+      <br />
+      <a
+        href="https://github.com/codestates-seb/seb43_main_004"
+        target="_blank"
+        rel="noreferrer"
+      >
+        프로젝트 깃허브 바로가기
+      </a>
     </FooterWrapper>
   )
 }
