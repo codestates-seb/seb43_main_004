40f31113af33c08bd451cc7d66aa1855d6d6ebb1
idx123 <jinhalee020@gmail.com>
2023-05-10 17:14:53 +0900

    feat : 로그인, 회원가입 페이지 경로 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import UserSignIn from './pages/UserSignIn'
+import UserSignUp from './pages/UserSignUp'
 
 function App() {
   return (
     <>
       <div className="App">
-        <Routes></Routes>
+        <Routes>
+          <Route path="/sign-in" element={<UserSignIn />} />
+          <Route path="/sign-up" element={<UserSignUp />} />
+        </Routes>
       </div>
     </>
   )
