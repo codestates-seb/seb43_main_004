b871b3b4cdd48e8f791f30c7852776f3c0c9be92
moondrop0816 <moondrop0816@gmail.com>
2023-05-12 15:56:35 +0900

    Design : 포지션 및 사이즈 조정

--- client/src/components/Common/Header.tsx
+++ client/src/components/Common/Header.tsx
@@ -8,6 +8,10 @@ const StyledHeader = styled.header`
   padding: 2rem;
   position: fixed;
   width: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
 
   .container {
     max-width: 1250px;
@@ -17,11 +21,12 @@ const StyledHeader = styled.header`
   }
 
   .btn-menu {
-    font-size: ${({ theme }) => theme.fontSize.lgl};
+    font-size: ${({ theme }) => theme.fontSize.lgh};
   }
 
   h1 {
     margin: 0 auto;
+    width: 10rem;
 
     img {
       display: block;
@@ -43,25 +48,23 @@ const StyledHeader = styled.header`
 `
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState<boolean>(false)
   const handleMenu = () => setMenuOpen(!menuOpen)
 
   return (
-    <>
-      <StyledHeader>
-        <div className="container">
-          <button className="btn-menu" onClick={handleMenu}>
-            <span className="material-icons-round">menu</span>
-          </button>
-          <h1>
-            <a href="/">
-              <img src={logo} alt="logo" />
-            </a>
-          </h1>
-        </div>
-      </StyledHeader>
+    <StyledHeader>
+      <div className="container">
+        <button className="btn-menu" onClick={handleMenu}>
+          <span className="material-icons-round">menu</span>
+        </button>
+        <h1>
+          <a href="/">
+            <img src={logo} alt="logo" />
+          </a>
+        </h1>
+      </div>
       <Nav menuOpen={menuOpen} handleMenu={handleMenu} />
-    </>
+    </StyledHeader>
   )
 }
 
