8a901e700b3579644d28bd2d8f57b289e1b92a54
idx123 <jinhalee020@gmail.com>
2023-05-14 01:53:50 +0900

    feat :  유저 페이지 경로 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,16 +7,23 @@ import UserFindPwd from './pages/UserFindPwd'
 import Header from './components/common/Header'
 import Footer from './components/common/Footer'
 import NotFound from './pages/NotFound'
+import UserPage from './pages/UserPage'
+import EditProfile from './components/User/EditProfile'
+import ChangePwd from './components/User/ChangePwd'
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <div className="App">
         <Routes>
           <Route path="/sign-in" element={<UserSignIn />} />
           <Route path="/sign-up" element={<UserSignUp />} />
           <Route path="/find-pwd" element={<UserFindPwd />} />
+          <Route path="/userpage" element={<UserPage />}>
+            <Route path="" element={<EditProfile />} />
+            <Route path="change-pwd" element={<ChangePwd />} />
+          </Route>
           <Route path="/*" element={<NotFound error="404" />} />
         </Routes>
       </div>
