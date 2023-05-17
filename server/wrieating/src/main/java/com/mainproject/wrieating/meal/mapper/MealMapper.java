036a639f2dca52d9ee65bc8a90c64bb7cde63bc1
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-05-17 16:32:48 +0900

    Update MealMapper.java

--- server/wrieating/src/main/java/com/mainproject/wrieating/meal/mapper/MealMapper.java
+++ server/wrieating/src/main/java/com/mainproject/wrieating/meal/mapper/MealMapper.java
@@ -5,8 +5,10 @@ import com.mainproject.wrieating.meal.dto.MealPostDto;
 import com.mainproject.wrieating.meal.entity.Meal;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Component
 public interface MealMapper {
     Meal mealPostDtoToMeal(MealPostDto mealPostDto);
 
