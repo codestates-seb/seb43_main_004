6c8ff3fa987a596d661f0d68dfaec2d39f52c575
021Skyfall <119563406+021Skyfall@users.noreply.github.com>
2023-05-17 10:58:09 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-# 빌드 파일의 이름이 콘텐츠와 다르다면 다음 줄의 .jar 파일 이름을 수정하시기 바랍니다.
-BUILD_JAR=$(ls /home/ec2-user/action/build/libs/deploy-0.0.1-SNAPSHOT.jar)
-JAR_NAME=$(basename $BUILD_JAR)
+
+# 빌드 디렉토리로 이동
+cd /home/ec2-user/action/server/wrieating/
 
 echo "> 현재 시간: $(date)" >> /home/ec2-user/action/deploy.log
 
-echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
+echo "> 프로젝트 빌드 시작" >> /home/ec2-user/action/deploy.log
+
+# Gradle Wrapper로 프로젝트 빌드
+./gradlew clean build >> /home/ec2-user/action/deploy.log 2>&1
+
+if [ $? -ne 0 ]; then
+  echo "> 빌드에 실패했습니다. 배포를 종료합니다." >> /home/ec2-user/action/deploy.log
+  exit 1
+fi
 
 echo "> build 파일 복사" >> /home/ec2-user/action/deploy.log
-DEPLOY_PATH=/home/ec2-user/action/
-cp $BUILD_JAR $DEPLOY_PATH
+DEPLOY_PATH=/home/ec2-user/action/server/wrieating/
+cp build/libs/*.jar $DEPLOY_PATH
+
+# 빌드된 JAR 파일 확인
+BUILD_JAR=$(ls $DEPLOY_PATH*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+
+echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
@@ -27,3 +41,5 @@ fi
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
 sudo nohup java -jar $DEPLOY_JAR >> /home/ec2-user/action/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+
+echo "> 배포가 완료되었습니다." >> /home/ec2-user/action/deploy.log
