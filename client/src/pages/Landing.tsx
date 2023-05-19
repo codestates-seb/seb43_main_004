a435246d3693a20f05b73a20949fdfeea7dcc0dc
moondrop0816 <moondrop0816@gmail.com>
2023-05-19 16:20:30 +0900

    Feat: 랜딩페이지 생성

---
+++ client/src/pages/Landing.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+const StyledLanding = styled.main``
+
+const Landing = () => {
+  return (
+    <StyledLanding>
+      <div>랜딩이에용</div>
+    </StyledLanding>
+  )
+}
+
+export default Landing
