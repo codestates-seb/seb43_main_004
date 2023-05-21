f1086f0e4f9cab32331b341f25713c1c28fc9dbd
021Skyfall <021skyfall@gmail.com>
2023-05-21 12:33:41 +0900

    dev/be-email_disable

--- server/wrieating/src/main/java/com/mainproject/wrieating/dataArchive/dbsource/DatabaseConnection.java
+++ server/wrieating/src/main/java/com/mainproject/wrieating/dataArchive/dbsource/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String DB_URL = "";
-    private static final String DB_USER = "";
-    private static final String DB_PASSWORD = "";
+    private static final String DB_URL = "jdbc:mysql://wrieating-database.ca59q4wbjg2d.ap-northeast-2.rds.amazonaws.com/wrieatingdb";
+    private static final String DB_USER = "admin";
+    private static final String DB_PASSWORD = "wrieating123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
