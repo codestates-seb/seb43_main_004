c456154a9641c464d515d155e14ab93a1a7bc489
021Skyfall <021skyfall@gmail.com>
2023-05-25 01:49:31 +0900

    dev/be/ref-member

--- server/wrieating/src/main/java/com/mainproject/wrieating/member/dto/MemberPatchPasswordDto.java
+++ server/wrieating/src/main/java/com/mainproject/wrieating/member/dto/MemberPatchPasswordDto.java
@@ -9,12 +9,12 @@ import javax.validation.constraints.Pattern;
 @Getter
 @Setter
 public class MemberPatchPasswordDto {
-    @NotNull
-    @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", message = "유효한 이메일 주소를 입력해주세요.")
+
     private String email;
-    @NotNull
+
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@!%*#?&])[A-Za-z\\d$@!%*#?&]{8,}$")
     private String curPassword;
+
     @NotNull
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@!%*#?&])[A-Za-z\\d$@!%*#?&]{8,}$")
     private String newPassword;
