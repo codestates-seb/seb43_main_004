780f1aa5ea46774bacad7f37059c4289c9db3a48
021Skyfall <021skyfall@gmail.com>
2023-05-30 00:22:04 +0900

    dev/be/changed_deploy

--- server/wrieating/src/main/java/com/mainproject/wrieating/dataArchive/dbsource/DatabaseConnection.java
+++ server/wrieating/src/main/java/com/mainproject/wrieating/dataArchive/dbsource/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String DB_URL = "jdbc:mysql://database-deploy.c4spq6pcsznh.ap-northeast-2.rds.amazonaws.com:13306/wrieatingdb";
-    private static final String DB_USER = "admin";
-    private static final String DB_PASSWORD = "Wr!eating1125";
+    private static final String DB_URL = "";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
